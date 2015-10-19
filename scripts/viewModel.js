602ef07da008e443a9ce6ca8310dada0c3a7a7b3
xuechenwong <xuechenwong@outlook.com>
2015-10-19 22:18:07 +0800

    test commit file with command line

---
+++ scripts/viewModel.js
@@ -0,0 +1,10 @@
+var ViewModel = function(){
+	var self = this;
+	self.getData = function(){
+		console.log("test get data");
+	}
+
+	self.parseData = function(data){
+		console.log("test for parse data");
+	}
+}
