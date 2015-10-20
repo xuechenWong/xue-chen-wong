495fd045b8e9a7fe373ab1934fb75c06d73b202f
wangxch <wangxch@jiazhengol.com>
2015-10-20 11:44:06 +0800

    add example code for apply and call method

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,13 @@
-$(function(){
-	console.log("test");
-})
+// $(function(){
+// 	console.log("test");
+// })
+
+function log(){
+	// arguments是伪数组，想使用数组的标准方法，可以通过Array.prototype.call()方法来实现
+	var args = Array.prototype.slice.call(arguments);
+	args.unshift('(app)');
+	console.log.apply(console,args);
+}
+
+log('1');
+log('1,2');
